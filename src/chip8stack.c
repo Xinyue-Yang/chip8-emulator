ee4edc22ad368e61f69cf97cce6d8ca5f8d3c733
Xinyue Yang <86961126+Xinyue-Yang@users.noreply.github.com>
2022-09-20 17:52:03 -0400

    Update chip8stack.c

--- src/chip8stack.c
+++ src/chip8stack.c
@@ -2,19 +2,23 @@
 #include "chip8.h"
 #include <assert.h>
 
+//checks if the depth of the stack is in bound
 static void chip8_stack_in_bounds(struct chip8* chip8)
 {
     //SP is unsigned, no need to check >=0
     assert(chip8->registers.SP < CHIP8_TOTAL_STACK_DEPTH);
 }
 
+//writes to the memory at current stack pointer. Pointer moves to next depth.
 void chip8_stack_push(struct chip8* chip8, unsigned short val)
 {
+    //checks if the depth of the stack is in bound. Exit if it's not.
     chip8_stack_in_bounds(chip8);
     chip8->stack.stack[chip8->registers.SP] = val;
     chip8->registers.SP++;
 }
 
+//pops the data at current stack pointer.
 unsigned short chip8_stack_pop(struct chip8* chip8)
 {
     chip8->registers.SP--;
